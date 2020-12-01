d4c61040a194da583e8d44ef2585381a101feafb
Kacper Królikowski <kaacperkrolikowski@gmail.com>
2020-12-01 15:27:33 +0100

    Getting data from api

--- app/src/main/java/com/krolikowski/covidmonitor/MainActivity.kt
+++ app/src/main/java/com/krolikowski/covidmonitor/MainActivity.kt
@@ -1,17 +1,21 @@
 package com.krolikowski.covidmonitor
 
 import android.os.Bundle
-import android.util.Log
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import okhttp3.OkHttpClient
-import okhttp3.Request
+import com.mashape.unirest.http.Unirest
+import org.jetbrains.anko.activityUiThread
 import org.jetbrains.anko.doAsync
+import org.json.JSONObject
 
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val mTextView: TextView = findViewById(R.id.textView)
 
         var selectedCountry = "Poland"
         var currentData = "2020-11-30"
@@ -19,18 +23,20 @@ class MainActivity : AppCompatActivity() {
         val covApiKey = BuildConfig.COV_API_KEY
 
         doAsync {
-            val client = OkHttpClient()
 
-            val request = Request.Builder()
-                .url("https://covid-19-data.p.rapidapi.com/report/country/name?date=$currentData&name=$selectedCountry")
-                .get()
-                .addHeader("x-rapidapi-key", covApiKey)
-                .addHeader("x-rapidapi-host", "covid-19-data.p.rapidapi.com")
-                .build()
+            var response =
+                    Unirest.get("https://covid-19-data.p.rapidapi.com/report/country/name?date=2020-04-01&name=Italy")
+                            .header("x-rapidapi-key", "c9016797e3mshcb3f479b86fc845p1fcc9djsnd7a2f17e2f37")
+                            .header("x-rapidapi-host", "covid-19-data.p.rapidapi.com")
+                            .asString()
 
-            val response = client.newCall(request).execute()
 
-            Log.d("HTTP", response.toString())
+            var all = response.body
+
+            val allres = JSONObject(all).getString("country")
+            activityUiThread {
+                mTextView.text = allres.toString()
+            }
 
         }
     }
--- app/src/main/java/com/krolikowski/covidmonitor/SingleCountryData.kt
+++ app/src/main/java/com/krolikowski/covidmonitor/SingleCountryData.kt
@@ -3,4 +3,4 @@ package com.krolikowski.covidmonitor
 
 import com.google.gson.annotations.SerializedName
 
-class SingleCountryData : ArrayList<SingleCountryDataItem>()
+class SingleCountryData(val data: List<SingleCountryData>)
