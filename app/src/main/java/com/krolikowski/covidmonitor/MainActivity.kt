b24572d0f28e6faa5f1977a038f99e1f235363ea
Kacper Królikowski <kaacperkrolikowski@gmail.com>
2020-12-02 17:40:04 +0100

    Gówno bez danych

--- app/src/main/java/com/krolikowski/covidmonitor/MainActivity.kt
+++ app/src/main/java/com/krolikowski/covidmonitor/MainActivity.kt
@@ -1,20 +1,18 @@
 package com.krolikowski.covidmonitor
 
+import android.app.DatePickerDialog
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.ImageButton
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import com.google.gson.Gson
 import com.google.gson.GsonBuilder
-import com.google.gson.JsonArray
-import com.google.gson.annotations.SerializedName
-import com.mashape.unirest.http.HttpResponse
 import com.mashape.unirest.http.Unirest
 import org.jetbrains.anko.activityUiThread
 import org.jetbrains.anko.doAsync
-import org.json.JSONObject
-import com.krolikowski.covidmonitor.SingleCountry
-import com.krolikowski.covidmonitor.SingleCountryItem
-
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -22,28 +20,25 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val mNameCountryTV: TextView = findViewById(R.id.country_name)
-        val mActiveCountryTV: TextView = findViewById(R.id.country_active)
-        val mDeathsCountryTV: TextView = findViewById(R.id.country_deaths)
-        val mRecoveredCountryTV: TextView = findViewById(R.id.country_recovered)
-        val mConfirmedCountryTV: TextView = findViewById(R.id.country_confirmed)
+        val mDataTV: TextView = findViewById(R.id.selected_data)
+        val mDataPickerButton: ImageButton = findViewById(R.id.select_data)
+        val mUpdateButton: Button = findViewById(R.id.update_button)
 
-        var selectedCountry = "Poland"
-        var currentData = "2020-11-30"
+        var selectedCountry = "Italy"
+        var selectedData = "2020-11-30"
 
-        val covApiKey = BuildConfig.COV_API_KEY
-
+        {
         //[{"country":"Italy","provinces":[{"province":"Italy","confirmed":110574,"recovered":16847,"deaths":13155,"active":80572}],"latitude":41.87194,"longitude":12.56738,"date":"2020-04-01"}]
 
-        doAsync {
+        /*doAsync {
 
-            var response =
+            val response =
                     Unirest.get("https://covid-19-data.p.rapidapi.com/report/country/name?date=2020-04-01&name=Italy")
                             .header("x-rapidapi-key", covApiKey)
                             .header("x-rapidapi-host", "covid-19-data.p.rapidapi.com")
                             .asString()
 
-            var countryInfoString = response.body.toString()
+            val countryInfoString = response.body.toString()
 
             val gson = GsonBuilder().create()
             val singleCountryInfo = gson.fromJson(countryInfoString, SingleCountry::class.java)
@@ -51,8 +46,77 @@ class MainActivity : AppCompatActivity() {
 
             activityUiThread {
 
-                mNameCountryTV.text = singleCountryInfo[0].country
+                (getString(R.string.country_name) + singleCountryInfo[0].country).also { mNameCountryTV.text = it }
+                (getString(R.string.active_case) + singleCountryInfo[0].provinces[0].active).also { mActiveCountryTV.text = it }
+                (getString(R.string.fatal_case) + singleCountryInfo[0].provinces[0].deaths.toString()).also { mDeathsCountryTV.text = it }
+                (getString(R.string.recovered_cases) + singleCountryInfo[0].provinces[0].recovered.toString()).also { mRecoveredCountryTV.text = it }
+                (getString(R.string.all_time_confirmed_cases) + singleCountryInfo[0].provinces[0].confirmed.toString()).also { mConfirmedCountryTV.text = it }
+                mDataTV.text = currentData
+            }
 
+        }*/
+    }
+
+        val calendar = Calendar.getInstance()
+        val year = calendar.get(Calendar.YEAR)
+        val month = calendar.get(Calendar.MONTH)
+        val day = calendar.get(Calendar.DAY_OF_MONTH)
+
+        val today = Calendar.getInstance().time
+        val formatter = SimpleDateFormat("yyyy-MM-dd")
+        val date: String = formatter.format(today)
+        mDataTV.text = date
+
+        updateData(selectedCountry, mDataTV.text.toString())
+
+        mDataPickerButton.setOnClickListener{
+            val dpd = DatePickerDialog(
+                this,
+                DatePickerDialog.OnDateSetListener { _, mYear, mMonth, mDay ->
+                    ("" + mYear + "-" + (mMonth+1) + "-" + mDay).also { mDataTV.text = it }
+                },
+                year,
+                month,
+                day
+            )
+            dpd.show()
+        }
+
+        mUpdateButton.setOnClickListener {
+            updateData(selectedCountry, mDataTV.text.toString())
+        }
+
+    }
+
+    private fun updateData(name: String, data: String){
+
+        val mNameCountryTV: TextView = findViewById(R.id.country_name)
+        val mActiveCountryTV: TextView = findViewById(R.id.country_active)
+        val mDeathsCountryTV: TextView = findViewById(R.id.country_deaths)
+        val mRecoveredCountryTV: TextView = findViewById(R.id.country_recovered)
+        val mConfirmedCountryTV: TextView = findViewById(R.id.country_confirmed)
+        val covApiKey = BuildConfig.COV_API_KEY
+
+        doAsync {
+
+            val response =
+                Unirest.get("https://covid-19-data.p.rapidapi.com/report/country/name?date=$data&name=$name")
+                    .header("x-rapidapi-key", covApiKey)
+                    .header("x-rapidapi-host", "covid-19-data.p.rapidapi.com")
+                    .asString()
+
+            val countryInfoString = response.body.toString()
+
+            val gson = GsonBuilder().create()
+            val singleCountryInfo = gson.fromJson(countryInfoString, SingleCountry::class.java)
+
+            activityUiThread() {
+
+                (getString(R.string.country_name) + singleCountryInfo[0].country).also { mNameCountryTV.text = it }
+                (getString(R.string.active_case) + singleCountryInfo[0].provinces[0].active).also { mActiveCountryTV.text = it }
+                (getString(R.string.fatal_case) + singleCountryInfo[0].provinces[0].deaths.toString()).also { mDeathsCountryTV.text = it }
+                (getString(R.string.recovered_cases) + singleCountryInfo[0].provinces[0].recovered.toString()).also { mRecoveredCountryTV.text = it }
+                (getString(R.string.all_time_confirmed_cases) + singleCountryInfo[0].provinces[0].confirmed.toString()).also { mConfirmedCountryTV.text = it }
             }
 
         }
---
+++ app/src/main/java/com/krolikowski/covidmonitor/SplashScreenActivity.kt
@@ -0,0 +1,31 @@
+package com.krolikowski.covidmonitor
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.view.View
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        supportActionBar!!.hide()
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+        actionBar?.hide()
+
+        val splashScreenDuration = getSplashScreenDuration()
+        Handler().postDelayed(
+            {
+                val intent = Intent(applicationContext, MainActivity::class.java)
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+                startActivity(intent)
+                finish()
+            },
+            splashScreenDuration
+        )
+    }
+
+    private fun getSplashScreenDuration() = 2500L
+}
