d9e026b08e6e99295adef32308a7f6b2422646a4
Kacper Królikowski <kaacperkrolikowski@gmail.com>
2020-12-03 02:16:48 +0100

    Pobieranie danych działa
    TODO:
    Wybór państwa
    Wygląd

--- app/src/main/java/com/krolikowski/covidmonitor/MainActivity.kt
+++ app/src/main/java/com/krolikowski/covidmonitor/MainActivity.kt
@@ -13,6 +13,7 @@ import org.jetbrains.anko.activityUiThread
 import org.jetbrains.anko.doAsync
 import java.text.SimpleDateFormat
 import java.util.*
+import kotlin.math.sin
 
 class MainActivity : AppCompatActivity() {
 
@@ -20,12 +21,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val mDataTV: TextView = findViewById(R.id.selected_data)
-        val mDataPickerButton: ImageButton = findViewById(R.id.select_data)
         val mUpdateButton: Button = findViewById(R.id.update_button)
 
         var selectedCountry = "Italy"
-        var selectedData = "2020-11-30"
 
         {
         //[{"country":"Italy","provinces":[{"province":"Italy","confirmed":110574,"recovered":16847,"deaths":13155,"active":80572}],"latitude":41.87194,"longitude":12.56738,"date":"2020-04-01"}]
@@ -57,52 +55,32 @@ class MainActivity : AppCompatActivity() {
         }*/
     }
 
-        val calendar = Calendar.getInstance()
-        val year = calendar.get(Calendar.YEAR)
-        val month = calendar.get(Calendar.MONTH)
-        val day = calendar.get(Calendar.DAY_OF_MONTH)
-
-        val today = Calendar.getInstance().time
-        val formatter = SimpleDateFormat("yyyy-MM-dd")
-        val date: String = formatter.format(today)
-        mDataTV.text = date
-
-        updateData(selectedCountry, mDataTV.text.toString())
-
-        mDataPickerButton.setOnClickListener{
-            val dpd = DatePickerDialog(
-                this,
-                DatePickerDialog.OnDateSetListener { _, mYear, mMonth, mDay ->
-                    ("" + mYear + "-" + (mMonth+1) + "-" + mDay).also { mDataTV.text = it }
-                },
-                year,
-                month,
-                day
-            )
-            dpd.show()
-        }
+        updateData(selectedCountry)
 
         mUpdateButton.setOnClickListener {
-            updateData(selectedCountry, mDataTV.text.toString())
+            updateData("USA")
         }
 
     }
 
-    private fun updateData(name: String, data: String){
+    private fun updateData(name: String){
 
         val mNameCountryTV: TextView = findViewById(R.id.country_name)
         val mActiveCountryTV: TextView = findViewById(R.id.country_active)
+        val mNewActiveCountryTV: TextView = findViewById(R.id.country_new_case)
         val mDeathsCountryTV: TextView = findViewById(R.id.country_deaths)
+        val mNewDeathCountryTV: TextView = findViewById(R.id.country_new_death)
         val mRecoveredCountryTV: TextView = findViewById(R.id.country_recovered)
         val mConfirmedCountryTV: TextView = findViewById(R.id.country_confirmed)
+        val mDataTV: TextView = findViewById(R.id.last_update_data)
         val covApiKey = BuildConfig.COV_API_KEY
 
         doAsync {
 
             val response =
-                Unirest.get("https://covid-19-data.p.rapidapi.com/report/country/name?date=$data&name=$name")
+                Unirest.get("https://covid-19-tracking.p.rapidapi.com/v1/$name")
                     .header("x-rapidapi-key", covApiKey)
-                    .header("x-rapidapi-host", "covid-19-data.p.rapidapi.com")
+                    .header("x-rapidapi-host", "covid-19-tracking.p.rapidapi.com")
                     .asString()
 
             val countryInfoString = response.body.toString()
@@ -110,13 +88,17 @@ class MainActivity : AppCompatActivity() {
             val gson = GsonBuilder().create()
             val singleCountryInfo = gson.fromJson(countryInfoString, SingleCountry::class.java)
 
-            activityUiThread() {
+            activityUiThread {
 
-                (getString(R.string.country_name) + singleCountryInfo[0].country).also { mNameCountryTV.text = it }
-                (getString(R.string.active_case) + singleCountryInfo[0].provinces[0].active).also { mActiveCountryTV.text = it }
-                (getString(R.string.fatal_case) + singleCountryInfo[0].provinces[0].deaths.toString()).also { mDeathsCountryTV.text = it }
-                (getString(R.string.recovered_cases) + singleCountryInfo[0].provinces[0].recovered.toString()).also { mRecoveredCountryTV.text = it }
-                (getString(R.string.all_time_confirmed_cases) + singleCountryInfo[0].provinces[0].confirmed.toString()).also { mConfirmedCountryTV.text = it }
+                (getString(R.string.country_name)+ " " + singleCountryInfo.countryText).also { mNameCountryTV.text = it }
+                (getString(R.string.active_case)+ " " + singleCountryInfo.activeCasesText).also { mActiveCountryTV.text = it }
+                (getString(R.string.new_active_case)+ " " + singleCountryInfo.newCasesText).also { mNewActiveCountryTV.text = it }
+                (getString(R.string.fatal_case)+ " " + singleCountryInfo.totalDeathsText).also { mDeathsCountryTV.text = it }
+                (getString(R.string.new_fatal_cases)+ " " + singleCountryInfo.newDeathsText).also { mNewDeathCountryTV.text = it }
+                (getString(R.string.recovered_cases)+ " " + singleCountryInfo.totalRecoveredText).also { mRecoveredCountryTV.text = it }
+                (getString(R.string.all_time_confirmed_cases)+ " " + singleCountryInfo.totalCasesText).also { mConfirmedCountryTV.text = it }
+                (getString(R.string.last_update)+ " " + singleCountryInfo.lastUpdate).also { mDataTV.text = it }
+
             }
 
         }
--- app/src/main/java/com/krolikowski/covidmonitor/SingleCountry.kt
+++ app/src/main/java/com/krolikowski/covidmonitor/SingleCountry.kt
@@ -3,4 +3,21 @@ package com.krolikowski.covidmonitor
 
 import com.google.gson.annotations.SerializedName
 
-class SingleCountry : ArrayList<SingleCountryItem>()
+data class SingleCountry(
+    @SerializedName("Active Cases_text")
+    val activeCasesText: String,
+    @SerializedName("Country_text")
+    val countryText: String,
+    @SerializedName("Last Update")
+    val lastUpdate: String,
+    @SerializedName("New Cases_text")
+    val newCasesText: String,
+    @SerializedName("New Deaths_text")
+    val newDeathsText: String,
+    @SerializedName("Total Cases_text")
+    val totalCasesText: String,
+    @SerializedName("Total Deaths_text")
+    val totalDeathsText: String,
+    @SerializedName("Total Recovered_text")
+    val totalRecoveredText: String
+)
