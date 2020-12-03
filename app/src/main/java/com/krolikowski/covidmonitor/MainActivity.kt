3399653e1dde93ee6ce7adb92b2c6d6becf80cb6
Kacper Królikowski <kaacperkrolikowski@gmail.com>
2020-12-03 18:47:16 +0100

    Pobieranie danych działa Trochę pracy z kotlinx RecycleView WIP
    TODO:
    Wybór państwa
    Wygląd

--- app/src/main/java/com/krolikowski/covidmonitor/MainActivity.kt
+++ app/src/main/java/com/krolikowski/covidmonitor/MainActivity.kt
@@ -18,49 +18,17 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         country_recycle_view.layoutManager = LinearLayoutManager(this)
-        country_recycle_view.adapter
+        country_recycle_view.adapter = MainAdapter()
 
-        country_list_fab.setOnClickListener(object : View.OnClickListener{
-            override fun onClick(v: View?) {
-                if(country_recycle_view.isVisible) {
-                    country_recycle_view.visibility = View.INVISIBLE
-                } else {
-                    country_recycle_view.visibility = View.VISIBLE
-                }
+        country_list_fab.setOnClickListener {
+            if (country_recycle_view.isVisible) {
+                country_recycle_view.visibility = View.INVISIBLE
+            } else {
+                country_recycle_view.visibility = View.VISIBLE
             }
-        })
+        }
 
         var selectedCountry = "Italy"
-
-        {
-        //[{"country":"Italy","provinces":[{"province":"Italy","confirmed":110574,"recovered":16847,"deaths":13155,"active":80572}],"latitude":41.87194,"longitude":12.56738,"date":"2020-04-01"}]
-
-        /*doAsync {
-
-            val response =
-                    Unirest.get("https://covid-19-data.p.rapidapi.com/report/country/name?date=2020-04-01&name=Italy")
-                            .header("x-rapidapi-key", covApiKey)
-                            .header("x-rapidapi-host", "covid-19-data.p.rapidapi.com")
-                            .asString()
-
-            val countryInfoString = response.body.toString()
-
-            val gson = GsonBuilder().create()
-            val singleCountryInfo = gson.fromJson(countryInfoString, SingleCountry::class.java)
-            println(countryInfoString)
-
-            activityUiThread {
-
-                (getString(R.string.country_name) + singleCountryInfo[0].country).also { mNameCountryTV.text = it }
-                (getString(R.string.active_case) + singleCountryInfo[0].provinces[0].active).also { mActiveCountryTV.text = it }
-                (getString(R.string.fatal_case) + singleCountryInfo[0].provinces[0].deaths.toString()).also { mDeathsCountryTV.text = it }
-                (getString(R.string.recovered_cases) + singleCountryInfo[0].provinces[0].recovered.toString()).also { mRecoveredCountryTV.text = it }
-                (getString(R.string.all_time_confirmed_cases) + singleCountryInfo[0].provinces[0].confirmed.toString()).also { mConfirmedCountryTV.text = it }
-                mDataTV.text = currentData
-            }
-
-        }*/
-    }
 
         updateData(selectedCountry)
 
@@ -89,7 +57,7 @@ class MainActivity : AppCompatActivity() {
 
             activityUiThread {
 
-                (getString(R.string.country_name)+ " " + singleCountryInfo.countryText).also { country_name.text = it }
+                (getString(R.string.country_name)+ " " + singleCountryInfo.countryText).also { country.text = it }
                 (getString(R.string.active_case)+ " " + singleCountryInfo.activeCasesText).also { country_active.text = it }
                 (getString(R.string.new_active_case)+ " " + singleCountryInfo.newCasesText).also { country_new_case.text = it }
                 (getString(R.string.fatal_case)+ " " + singleCountryInfo.totalDeathsText).also { country_deaths.text = it }
--- app/src/main/java/com/krolikowski/covidmonitor/MainAdapter.kt
+++ app/src/main/java/com/krolikowski/covidmonitor/MainAdapter.kt
@@ -1,20 +1,27 @@
 package com.krolikowski.covidmonitor
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.country_card.view.*
 
 class MainAdapter: RecyclerView.Adapter<CustomViewHolder>() {
     override fun getItemCount(): Int {
+        return 222
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CustomViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val single = layoutInflater.inflate(R.layout.country_card, parent, false)
+        return CustomViewHolder(single)
     }
 
     override fun onBindViewHolder(holder: CustomViewHolder, position: Int) {
+        holder.view.country_name.text = "Hejka"
     }
 }
 
-class CustomViewHolder(v: View): RecyclerView.ViewHolder(v){
+class CustomViewHolder(val view: View): RecyclerView.ViewHolder(view){
 
 }
